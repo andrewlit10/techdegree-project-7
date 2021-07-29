54eb1a63a38f7f9d029cf4a8a84451f6d72b9a96
andrewlit10 <63842626+andrewlit10@users.noreply.github.com>
2021-07-28 20:48:14 -0700

    update, finishing media queries

--- js/app.js
+++ js/app.js
@@ -1,131 +1,29 @@
+const bellAlert = document.getElementById("bell-dot");
+const bellMessage = document.getElementById("bellMessage");
+const dotMessage = document.getElementById("bell-dot");
 const alertBanner = document.getElementById("alert");
-const dailyCanvas = document.getElementById("daily-chart");
-const mobileCanvas = document.getElementById("mobile-chart");
-const user = document.getElementById("userField");
-const message = document.getElementById("messageField");
-const send = document.getElementById("send");
-// create the HTML for the banner
 
+// create the html for the Banner
 alertBanner.innerHTML =
- `
- <div class="alert-banner">
- <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
- <p class="alert-banner-close">x</p>
- </div>
- `
-alertBanner.addEventListener('click', e => {
-  const element = e.target;
-  if (element.classList.contains("alert-banner-close")) {
-    alertBanner.style.display = "none"
-  }
-
-});
-let trafficData = {
-  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31",],
-  datasets: [{
-    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-    backgroundColor: 'rgba(116, 119, 191, .3)',
-    borderWidth: 1,
-  }]
-};
-
-let trafficOptions = {
-  aspectRatio: 2.5,
-  animation: {
-    duration: 0
-  },
-  scales: {
-    y: {
-      beginAtZero: true
+`
+<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+    </div>
+`
+alertBanner.addEventListener('click', e =>{
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")){
+        alertBanner.style.display = "none"
     }
-  },
-  plugins: {
-    legend: {
-      display: false
-    }
-  }
-};
-
-
-
-let trafficChart = new Chart(trafficCanvas, {
-  type: 'line',
-  data: trafficData,
-  options: trafficOptions
-
 });
 
-// data for the daily traffic bar chart
-const dailyData = {
-  labels: ["S", "M", "T", "W", "T", "F", "S"],
-  datasets: [{
-    label: '# of Hits',
-    data: [75, 115, 175, 125, 225, 200, 100],
-    backgroundColor: '#7477BF',
-    borderWidth: 1
-  }]
-};
-
-const dailyOptions = {
-  scaled: {
-    y: {
-      beginAtZero: true
-    }
-  },
-  plugins: {
-    legend: {
-      display: false
-    }
+// Notification icon
+bellAlert.addEventListener('click', e => {
+  if(e.target === bellMessage || e.target === dotMessage){
+    window.alert('Call your mom back.');
+    window.alert('Do a peer review today.');
+    dotMessage.style.display = 'none';
   }
-};
-let dailyChart = new Chart(dailyCanvas, {
-  type: 'bar',
-  data: dailyData,
-  options: dailyOptions
-});
-
-const mobileData = {
-  labels: ["Desktop", "Tablet", "Phone"],
-  datasets: [{
-    label: '# of Users',
-    data: [2000, 550, 500],
-    borderWidth: 0,
-    backgroundColor: [
-      '#7477BF',
-      '#78CF82',
-      '#51B6C8'
-    ]
-  }]
-};
-
-const mobileOptions = {
-  plugins: {
-    legend: {
-      position: 'right',
-      labels: {
-        boxwidth: 20,
-        fontStyle: 'bold'
-      }
-    }
-  }
-};
-
-let mobileChart = new Chart(mobileCanvas, {
-  type: 'donut',
-  data: mobileData,
-  options: mobileOptions
-});
-
-
-send.addEventListener('click', () => {
-  //ensure user and message fields filled out
-  if (user.value === "" && message.value === "" ) {
-    alert("Please fill out user and message fields before sending");
-  } else if (user.value === "" ) {
-    alert("Please fill out user field before sending")
-  } else if (message.value === "" ) {
-    alert("Please fill out message field before sending");
-  } else {
-    alert(`Message successfully sent to: ${user.value}`)
-  }
+  // Pop-ups are ugly, will come back later and make it a more appropriate notification style
 });
